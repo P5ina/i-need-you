ae76af10a911ef49a737ec92f4b54173019e6c38
Akakiy <runtimeerror57@gmail.com>
2024-07-09 20:50:17 +0300

    cumpfire zvuki

--- scripts/campfire.gd
+++ scripts/campfire.gd
@@ -7,16 +7,17 @@ extends Node3D
 @export var light_change_duration := 0.5
 
 var is_on := false
-var tween : Tween
+var tween: Tween
 
-@onready var fire_particles : GPUParticles3D = $FireParticles
-@onready var fire_light : OmniLight3D = $FireLight
+@onready var fire_particles: GPUParticles3D = $FireParticles
+@onready var fire_light: OmniLight3D = $FireLight
+@onready var campfire_loop: AudioStreamPlayer = $CampfireLoop
+@onready var campfire_ignite: AudioStreamPlayer = $CampfireIgnite
 
 
 func _ready() -> void:
 	fire_particles.emitting = false
 	fire_light.light_energy = 0.0
-	turn_on()
 
 
 func rand_light() -> float:
@@ -26,14 +27,18 @@ func rand_light() -> float:
 func _process(delta: float) -> void:
 	if is_on == false:
 		return
-	if tween.is_running():
-		return
-	tween = create_tween()
-	tween.tween_property(fire_light, "light_energy", rand_light(), light_change_duration)
+
+	if not tween.is_running():
+		tween = create_tween()
+		tween.tween_property(fire_light, "light_energy", rand_light(), light_change_duration)
 
 
 func turn_on() -> void:
 	is_on = true
 	fire_particles.emitting = true
+
 	tween = create_tween()
 	tween.tween_property(fire_light, "light_energy", rand_light(), light_start_duration)
+
+	campfire_ignite.play()
+	campfire_loop.play()
