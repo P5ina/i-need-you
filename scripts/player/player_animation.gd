8f8a316a037268e3e48ca9e3df43a1a8a1025f9d
Timur Turatbekov <timatur242005@gmail.com>
2024-11-04 21:48:33 +0600

    Add player animations

---
+++ scripts/player/player_animation.gd
@@ -0,0 +1,48 @@
+extends AnimatedSprite2D
+
+enum AnimationDirection {
+	UP,
+	RIGHT,
+	DOWN,
+	LEFT,
+}
+
+var last_direction: AnimationDirection
+var last_is_moving: bool
+
+@onready var interaction_area: InteractionArea = owner.get_meta(InteractionArea.META_NAME)
+
+
+func _process(_delta: float) -> void:
+	var normalized_velocity: Vector2 = (owner as CharacterBody2D).velocity.normalized()
+	var direction: AnimationDirection = get_direction(normalized_velocity)
+	var is_moving: bool = normalized_velocity.length() > 0.1
+
+	update_animation(direction, is_moving)
+
+func get_direction(normalized_velocity: Vector2) -> AnimationDirection:
+	if normalized_velocity.y < 0:
+		return AnimationDirection.UP
+	elif normalized_velocity.y > 0:
+		return AnimationDirection.DOWN
+	elif normalized_velocity.x > 0:
+		return AnimationDirection.RIGHT
+	elif normalized_velocity.x < 0:
+		return AnimationDirection.LEFT
+
+	return last_direction if last_direction != null else AnimationDirection.DOWN
+
+
+func update_animation(direction: AnimationDirection, is_moving: bool) -> void:
+	if direction == last_direction and is_moving == last_is_moving:
+		return
+
+	last_direction = direction
+	last_is_moving = is_moving
+	play(direction_to_animation_name(direction, is_moving))
+
+
+func direction_to_animation_name(direction: AnimationDirection, is_moving: bool) -> String:
+	var direction_name: String = ["up", "right", "down", "left"][direction]
+	var state: String = "walk" if is_moving else "idle"
+	return state + "_" + direction_name
