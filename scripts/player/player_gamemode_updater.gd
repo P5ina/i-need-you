2a7399a565998c69f67bcab026d7ba7aa8f98615
Timur Turatbekov <timatur242005@gmail.com>
2024-11-05 00:37:33 +0600

    Add looking vulture

---
+++ scripts/player/player_gamemode_updater.gd
@@ -0,0 +1,4 @@
+extends Node
+
+func _ready() -> void:
+	Gamemode.current_player = owner
--- scripts/story/vulture/vulture_looking_rotation.gd
+++ scripts/story/vulture/vulture_looking_rotation.gd
@@ -1,11 +1,36 @@
 extends Sprite2D
 
+const LOOK_DIRECTION_DOWN = 0
+const LOOK_DIRECTION_DOWN_RIGHT = 1
+const LOOK_DIRECTION_RIGHT = 2
+const LOOK_DIRECTION_UP_RIGHT = 3
+const LOOK_DIRECTION_UP = 4
+const LOOK_DIRECTION_UP_LEFT = 5
+const LOOK_DIRECTION_LEFT = 6
+const LOOK_DIRECTION_DOWN_LEFT = 7
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _process(_delta: float) -> void:
+	var direction: Vector2 = global_position.direction_to(Gamemode.current_player.global_position)
+	var angle: float = direction.angle()
+	var new_frame: int = LOOK_DIRECTION_RIGHT
 
+	if angle < deg_to_rad(-162):
+		new_frame = LOOK_DIRECTION_LEFT
+	elif angle < deg_to_rad(-126):
+		new_frame = LOOK_DIRECTION_UP_LEFT
+	elif angle < deg_to_rad(-54):
+		new_frame = LOOK_DIRECTION_UP
+	elif angle < deg_to_rad(-18):
+		new_frame = LOOK_DIRECTION_UP_RIGHT
+	elif angle < deg_to_rad(18):
+		new_frame = LOOK_DIRECTION_RIGHT
+	elif angle < deg_to_rad(54):
+		new_frame = LOOK_DIRECTION_DOWN_RIGHT
+	elif angle < deg_to_rad(126):
+		new_frame = LOOK_DIRECTION_DOWN
+	elif angle < deg_to_rad(162):
+		new_frame = LOOK_DIRECTION_DOWN_LEFT
+	else:
+		new_frame = LOOK_DIRECTION_LEFT
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	frame = new_frame
