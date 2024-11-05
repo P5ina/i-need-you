1a38d30ce10ab3968e95802bd3c6113f294de373
Timur Turatbekov <timatur242005@gmail.com>
2024-11-05 15:36:39 +0600

    Add transition ot story from vulture dialogue

--- scripts/autoloads/story_state/story_state.gd
+++ scripts/autoloads/story_state/story_state.gd
@@ -2,7 +2,16 @@ extends Node
 
 const SAVE_FILE = "user://story.save"
 
+enum CharacterState {
+	NONE,
+	BEGINING,
+	STORY,
+	CONVINCE,
+	ENDING,
+}
+
 var intro_played: bool = false
+var vulture_state: CharacterState = CharacterState.NONE
 
 func _ready() -> void:
 	load_state()
@@ -10,10 +19,17 @@ func _ready() -> void:
 
 func get_save_state() -> Dictionary:
 	return {
-		"intro_played": intro_played
+		"intro_played": intro_played,
+		"vulture_state": vulture_state,
 	}
 
 
+func set_save_state(state: Dictionary) -> void:
+	intro_played = state["intro_played"]
+	vulture_state = state["vulture_state"]
+
+
+# TODO: Add save for dialogue lines to prevent abusing the save system
 func save_state() -> void:
 	var save_file: FileAccess = FileAccess.open(SAVE_FILE, FileAccess.WRITE)
 	var save: Dictionary = get_save_state()
@@ -24,15 +40,15 @@ func save_state() -> void:
 func load_state() -> void:
 	if not FileAccess.file_exists(SAVE_FILE):
 		return
-	
+
 	var parser: JSON = JSON.new()
-	
+
 	var save_file: FileAccess = FileAccess.open("user://story.save", FileAccess.READ)
 	var json_string: String = save_file.get_line()
 	var parse_error: Error = parser.parse(json_string)
 	if parse_error != OK:
-		push_error("JSON Parse Error: ", parser.get_error_message(), 
+		push_error("JSON Parse Error: ", parser.get_error_message(),
 			" in ", json_string, " at line ", parser.get_error_line())
-	
+
 	var save: Dictionary = parser.data
-	intro_played = save["intro_played"]
+	set_save_state(save)
--- scripts/character/character_interactable.gd
+++ scripts/character/character_interactable.gd
@@ -2,17 +2,25 @@ class_name CharacterInteractable
 extends Interactable
 
 @export var facing_direction: PlayerAnimation.AnimationDirection
+@export_file("*.tscn") var story_scene: String
 
 @onready var character_camera_2d: PhantomCamera2D = $CharacterCamera2D
 @onready var player_position: Node2D = $PlayerPosition
 
 
 func interact(_player: CharacterBody2D) -> void:
+	StoryState.vulture_state = StoryState.CharacterState.BEGINING
 	character_camera_2d.priority = 50
 	_player.get_meta(PlayerAnimation.META_NAME).update_animation(facing_direction, false)
 	_player.global_position = player_position.global_position
 	Dialogic.start("vulture_begining")
 	await Dialogic.timeline_ended
 
-	character_camera_2d.priority = 0
-	interaction_ended.emit()
+	if Dialogic.VAR.get(&"load_story"):
+		Dialogic.VAR.set(&"load_story", false)
+		StoryState.vulture_state = StoryState.CharacterState.STORY
+		SceneLoader.transit_to_scene(story_scene)
+	else:
+		StoryState.vulture_state = StoryState.CharacterState.NONE
+		character_camera_2d.priority = 0
+		interaction_ended.emit()
